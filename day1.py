8e241c326fa22d294701333230d1b06232983e16
Yaparla Usharani <ushareddyy78@gmail.com>
2025-09-19 19:51:17 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,49 @@
+# 1 Program to read 2 numbers as input and find summation
+# a = int(input('Enter first number: '))
+# b = int(input('Enter second number'))
+# print(f"Summation : {a + b}")
+
+#write a python program to read an integer value and check whether it is a digit or number
+# num = int(input('Enter a number'))
+# #if-else
+# if (num>=-9 and num<=9):
+#     print('Digit')
+# else :
+#     print('Number')
+# #ternary operator
+# res = "Digit" if(num>=-9 and num <=9) else "Number"
+# print(res)
+
+
+#write a python program to read an integer as input from user and check whether it is a 2 digit number or not a 2 digit number
+
+# dig = int(input('Enter a number : '))
+# res = " 2 Digit number" if ((dig >= -99 and dig <= -10 ) or (dig >= 10 and dig <= 99) ) else "Number"
+# print(res)
+ 
+#
+
+# write a python program to read an integer as input from the user  & check whether it is a three digit number or not?
+
+# dig = int(input('Enter a number : '))
+# res = " 3 Digit number" if ((dig >= -999 and dig < -99 ) or (dig > 99 and dig <= 999) ) else "not a 3 digit Number"
+# print(res)
+
+#write a python program to read an integer as input from the user & check whether it is a four digit number r not
+
+# dig = int(input('Enter a number : '))
+# res = " 4 Digit number" if ((dig >= -9999 and dig <= -1000 ) or (dig >= 1000 and dig <= 9999) ) else "not a 4 digit Number"
+# print(res)
+
+# write a python Program to read 2 integer values as input and find largerst using if-else and turnary
+
+# a = int(input('Enter first number: '))
+# b = int(input('Enter second number'))
+# #if - else
+# if (a>b):
+#     print(f"{a} is the largest")
+
+# else:
+#     print(f"{b} is the largest")
+# #ternary operator
+
